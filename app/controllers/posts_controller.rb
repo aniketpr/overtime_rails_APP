3f4a7dd4dd14ffa2d9a37424839e862de9f24e0b
aniketprr <pr.aniket1994@gmail.com>
2021-03-06 13:01:51 +0530

    updated

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,35 @@
+class PostsController < ApplicationController
+
+	before_action :set_post, only: [:show]
+
+	def index
+		
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+	  @post = Post.new(post_parmas)
+	  if @post.save
+	  	redirect_to @post # redirect to show page
+	  else
+	  	render :new
+	  end
+	end
+
+	def show	
+	end
+
+	private
+
+	def post_parmas
+		params.require(:post).permit(:date, :rationale)
+	end
+
+	def set_post
+		@post = Post.find(params[:id])
+	end
+
+end
